380bab6ef489b80ca7e61553f6b7eb300fa8ccdb
MOND3J <mposible99@gmail.com>
2025-09-15 12:40:27 +0200

    Typy proste

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // tu program główny
+        System.out.println("dzień dobry");
+
+        //typy proste
+        //pisane z małej litery, zmienne a nie obiekty -> nie mają pól i metod
+        //typy całkowite
+        byte liczba1 = 100;
+        short liczba2 = 300;
+        int liczba3 = 1234565;
+        long liczba4 = 436222749;
+        System.out.println("Twoja liczba " + liczba1);
+
+        //typy zmiennoprzecinkowe, rzeczywiste, z ułamkiem
+        float liczbaRzeczywista1 = 3.14f;
+        double liczbaRzeczywista2 = 23.4564;
+        System.out.println("Liczba rzeczywista " + liczbaRzeczywista2);
+
+        //typ logiczny
+        boolean czyPrawda = true;
+
+        //typ znakowy
+        char znak = 'a';
+
+
     }
 }
